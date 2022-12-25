7c1bfb688f0893b7d993d60716de8e454c1202b2
AhmedS <geekahmeds@gmail.com>
2022-12-25 18:14:55 +0400

    finishing form and add to json files

--- src/Create.js
+++ src/Create.js
@@ -1,25 +1,63 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [content, setBody] = useState('');
+    const [author, setAuthor] = useState('---');
+    const [isPending, setIsPending] = useState(false)
+
+
+    
+    const handelSubmit =(e) => {
+            e.preventDefault();
+        const blog = {title, content, author}
+        
+        setIsPending(true);
+        fetch('http://localhost:7000/blogs', {
+            method: 'POST', 
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(blog)
+        }).then(() => {
+            console.log('new Blog Added');
+            setIsPending(false);
+        })
+        
+    }
+
+
     return (
 
         <div className="create">
 
-            <form>
+            <form onSubmit={handelSubmit}>
             <label htmlFor="">Title</label>
                 <input
                      type="text"
                      required
+                     value={title}
+                     onChange={(e) => setTitle(e.target.value) }
                  />
                 <label htmlFor="">Content</label>
                 <textarea name="content" id="content" cols="30" rows="10"
                         required
+                        value={content}
+                        onChange={(e) => setBody(e.target.value) }
+   
                 ></textarea>
                 <label htmlFor="">Author</label>
-                <select name="author" id="author">
+                <select name="author" id="author"
+                        value={author}
+                        onChange={(e) => setAuthor(e.target.value)}
+                        >
                     <option value="blank">---</option>
                     <option value="ahmeds">AhmedS</option>
                     <option value="mohameds">MohamedS</option>
                 </select>
-                <button>Add Blog</button>
+                { !isPending && <button>Add Blog</button> }
+                { isPending  && <button>Adding ...</button> }
+                <h2>{title}</h2>
+                <p>{content}</p>
+                <p>{author}</p>
             </form>
 
         </div>
