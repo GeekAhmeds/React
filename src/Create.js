97b7cf97cc6fdc463e1fc1c914a96fbbb934b3b4
AhmedS <geekahmeds@gmail.com>
2022-12-25 17:28:20 +0400

    add create form

--- src/Create.js
+++ src/Create.js
@@ -2,7 +2,25 @@ const Create = () => {
     return (
 
         <div className="create">
-            <h2>Add a New Blog</h2>
+
+            <form>
+            <label htmlFor="">Title</label>
+                <input
+                     type="text"
+                     required
+                 />
+                <label htmlFor="">Content</label>
+                <textarea name="content" id="content" cols="30" rows="10"
+                        required
+                ></textarea>
+                <label htmlFor="">Author</label>
+                <select name="author" id="author">
+                    <option value="blank">---</option>
+                    <option value="ahmeds">AhmedS</option>
+                    <option value="mohameds">MohamedS</option>
+                </select>
+                <button>Add Blog</button>
+            </form>
 
         </div>
 
